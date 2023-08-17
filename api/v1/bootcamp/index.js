879c28c52a50cb390c6ce8660ef3bae296500a58
dvjhr <2dava.aditya21@gmail.com>
2023-08-17 20:19:59 +0700

    first commit

---
+++ api/v1/bootcamp/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+
+const mongo = require('mongojs');
+const db = mongo('mongodb+srv://mntbdjw:mntbdjw@csr.rur1bj0.mongodb.net/CSR', ['todos'])
+
+// router.get('/', function(req, res, next){
+//     res.send('Hello, World!')
+// });
+
+router.get('/', function(req, res, next){
+
+    db.todos
+    .find({},function(err, result){
+        if(err){
+            res.send(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+router.get('/:id', function(req, res, next){
+
+    let query = {
+        _id: db.ObjectId(req.params.id)
+    };
+
+    db.todos
+    .findOne(query, function(err, result){
+        if(err){
+            res.send(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+router.post('/', function(req, res, next){
+
+    let todo = req.body;
+
+    if(!todo.text || !(todo.isCompleted + '')){
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        })
+    } else {
+        db.todos
+        .save(todo, function(err, result){
+            if(err){
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        });
+
+    }
+});
+
+router.put('/:id', function(req, res, next){
+
+    let todo = req.body;
+
+    if(!todo.text || !(todo.isCompleted + '')){
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        })
+    } else {
+        db.todos
+        .replaceOne({
+            _id: db.ObjectId(req.params.id)
+        }, todo, {}, function(err, result){
+            if(err){
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        });
+
+    }
+});
+
+
+
+
+module.exports = router;
